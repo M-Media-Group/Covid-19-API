6dfb4321fe8de1096e6553406ea39469d33b75a3
mwargan <mwargan@gmail.com>
2020-03-25 20:53:34 +0000

    Apply fixes from StyleCI

--- lambda_functions/history/lambda_function.py
+++ lambda_functions/history/lambda_function.py
@@ -9,88 +9,100 @@ from get_countries import proccessCountries
 
 country_array = proccessCountries()
 global_population = 0
-latest_url ='https://opendata.arcgis.com/datasets/bbb2e4f589ba40d692fab712ae37b9ac_1.csv'
+latest_url = 'https://opendata.arcgis.com/datasets/bbb2e4f589ba40d692fab712ae37b9ac_1.csv'
+
 
 def proccessMain(status):
-	url ='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_'+status+'_global.csv'
-	data = {}
-	converted_dates = {}
+    url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_' + status + '_global.csv'
+    data = {}
+    converted_dates = {}
 
-	with closing(get(url, stream=True)) as r:
-		reader = csv.DictReader(codecs.iterdecode(r.iter_lines(), 'utf-8'))
-		rows = list(reader)
+    with closing(get(url, stream=True)) as r:
+        reader = csv.DictReader(codecs.iterdecode(r.iter_lines(), 'utf-8'))
+        rows = list(reader)
 
-	for row in rows:
-		area_sum = 0
+    for row in rows:
+        area_sum = 0
 
-		if (row['Country/Region'] not in data):
-			data[row['Country/Region']] = {}
-			data[row['Country/Region']]['All'] = {}
-			if(row['Country/Region'] in country_array):
-				for key in country_array[row['Country/Region']]:
-					data[row['Country/Region']]['All'][key] = country_array[row['Country/Region']][key]
-			
-			data[row['Country/Region']]['All']['dates'] = {}
+        if (row['Country/Region'] not in data):
+            data[row['Country/Region']] = {}
+            data[row['Country/Region']]['All'] = {}
+            if (row['Country/Region'] in country_array):
+                for key in country_array[row['Country/Region']]:
+                    data[row['Country/Region']]['All'][key] = country_array[
+                        row['Country/Region']][key]
 
-		if row['Province/State'] == '':
-			row['Province/State'] = "All"
+            data[row['Country/Region']]['All']['dates'] = {}
 
-		if (row['Province/State'] not in data[row['Country/Region']]):
-			data[row['Country/Region']][row['Province/State']] = {}
+        if row['Province/State'] == '':
+            row['Province/State'] = "All"
 
-		if row['Province/State'] == row['Country/Region']:
-			data[row['Country/Region']]['All']['lat'] = row['Lat']
-			data[row['Country/Region']]['All']['long'] = row['Long']
+        if (row['Province/State'] not in data[row['Country/Region']]):
+            data[row['Country/Region']][row['Province/State']] = {}
 
+        if row['Province/State'] == row['Country/Region']:
+            data[row['Country/Region']]['All']['lat'] = row['Lat']
+            data[row['Country/Region']]['All']['long'] = row['Long']
 
-		if ('dates' not in data[row['Country/Region']][row['Province/State']]):
-			data[row['Country/Region']][row['Province/State']]['dates'] = {}
+        if ('dates' not in data[row['Country/Region']][row['Province/State']]):
+            data[row['Country/Region']][row['Province/State']]['dates'] = {}
 
-		for column in reversed(row):
-			if column != 'Province/State' and column != 'Country/Region' and column != 'Lat' and column != 'Long':
-				# total_collected_sum = total_collected_sum + int(row[column])
-				original_column = column
-				if(row[column] == ''):
-					row[column] = 0
+        for column in reversed(row):
+            if column != 'Province/State' and column != 'Country/Region' and column != 'Lat' and column != 'Long':
+                # total_collected_sum = total_collected_sum + int(row[column])
+                original_column = column
+                if (row[column] == ''):
+                    row[column] = 0
 
-				if(column not in converted_dates):
-					converted_dates[column] = datetime.datetime.strptime(column, '%m/%d/%Y').strftime('%F')
+                if (column not in converted_dates):
+                    converted_dates[column] = datetime.datetime.strptime(
+                        column, '%m/%d/%Y').strftime('%F')
 
-				column = converted_dates[column]
+                column = converted_dates[column]
 
-				if (column not in data[row['Country/Region']]['All']['dates']):
-					data[row['Country/Region']]['All']['dates'][column] = 0
+                if (column not in data[row['Country/Region']]['All']['dates']):
+                    data[row['Country/Region']]['All']['dates'][column] = 0
 
-				data[row['Country/Region']]['All']['dates'][column] = data[row['Country/Region']]['All']['dates'][column] + int(float(row[original_column]))
+                data[row['Country/Region']]['All']['dates'][column] = data[
+                    row['Country/Region']]['All']['dates'][column] + int(
+                        float(row[original_column]))
 
-				if (column not in data[row['Country/Region']][row['Province/State']]['dates']):
-					data[row['Country/Region']][row['Province/State']]['dates'][column] = 0
+                if (column not in data[row['Country/Region']][
+                        row['Province/State']]['dates']):
+                    data[row['Country/Region']][
+                        row['Province/State']]['dates'][column] = 0
 
-				data[row['Country/Region']][row['Province/State']]['dates'][column]  = int(row[original_column])
+                data[row['Country/Region']][
+                    row['Province/State']]['dates'][column] = int(
+                        row[original_column])
 
-	global_array = {'All': {'population': country_array['Global']['population'], 'dates': {}}}
+    global_array = {
+        'All': {
+            'population': country_array['Global']['population'],
+            'dates': {}
+        }
+    }
 
-	for (key, value) in data.items():
+    for (key, value) in data.items():
 
-		for column in value['All']['dates']:
-			if (column not in global_array['All']['dates']):
-				global_array['All']['dates'][column] = 0
-			global_array['All']['dates'][column] = global_array['All']['dates'][column] + value['All']['dates'][column]
+        for column in value['All']['dates']:
+            if (column not in global_array['All']['dates']):
+                global_array['All']['dates'][column] = 0
+            global_array['All']['dates'][column] = global_array['All'][
+                'dates'][column] + value['All']['dates'][column]
 
-	data['Global'] = global_array
-	return data
+    data['Global'] = global_array
+    return data
+
 
 def lambda_handler(event, context):
 
-	data = proccessMain(event['queryStringParameters']['status'].lower())
+    data = proccessMain(event['queryStringParameters']['status'].lower())
 
-	try:
-		return_data = data[event['queryStringParameters']['country']]
+    try:
+        return_data = data[event['queryStringParameters']['country']]
 
-	except:
-		return_data = data  # or whatever
-	
-	return {
-		'statusCode': 200,
-		'body': json.dumps(return_data)
-	}
+    except:
+        return_data = data  # or whatever
+
+    return {'statusCode': 200, 'body': json.dumps(return_data)}
